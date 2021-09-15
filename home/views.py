db3ea9adaa284de7f2f096ea176368a827b2d57f
Jayesh <jayeshkaushik2@gmail.com>
2021-09-15 23:45:43 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,46 @@
+from django.db.models.expressions import Value
+from django.shortcuts import redirect, render
+from home.models import Room, Message
+from django.http import HttpResponse, JsonResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def room(request, room):
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name=room)
+    return render(request, 'index.html', {
+        'username':username,
+        'room': room,
+        'room_details': room_details
+    })
+
+
+def checkview(request):
+    room = request.POST['roomname']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists():
+        return redirect('/'+room+'/?username='+username)
+    
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect('/'+room+'/?username='+username)
+
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+    new_message = Message.objects.create(value=message, user=username, room=room_id)
+    new_message.save()
+    return  HttpResponse('message sent!')
+
+
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+    messages = Message.objects.filter(room=room_details.id)
+    messages = list(messages.values())
+    return JsonResponse({"messages":messages})
